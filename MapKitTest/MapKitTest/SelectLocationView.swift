e7364a16187a3c003651bbcd5179188ab0c49553
JIN-JJS <joonsu2005@naver.com>
2024-03-25 16:38:46 +0900

    [Add] SelectLocationView

---
+++ MapKitTest/MapKitTest/SelectLocationView.swift
@@ -0,0 +1,18 @@
+//
+//  SelectLocationView.swift
+//  MapKitTest
+//
+//  Created by 전준수 on 3/25/24.
+//
+
+import SwiftUI
+
+struct SelectLocationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SelectLocationView()
+}
