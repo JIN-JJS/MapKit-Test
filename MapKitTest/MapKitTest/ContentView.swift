67d5d5b6b9ca2e3b7c33ca8058717157f7c59b10
JIN-JJS <joonsu2005@naver.com>
2024-03-26 06:10:05 +0900

    [Del] TabView Delete

--- MapKitTest/MapKitTest/ContentView.swift
+++ MapKitTest/MapKitTest/ContentView.swift
@@ -9,13 +9,9 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        TabView(selection: .constant(2),
-                content:  {
-            LocationSearchView().tabItem { Image(systemName: "magnifyingglass.circle.fill")
-                Text("위치 검색") }.tag(1)
-            SelectLocationView().tabItem { Image(systemName: "paperplane.circle")
-                Text("선택 위치") }.tag(2)
-        })
+        NavigationStack {
+            SelectLocationView()
+        }
     }
 }
 
