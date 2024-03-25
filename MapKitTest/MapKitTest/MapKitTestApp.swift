3127e01e121a4652917ef6f3332c71506daee33e
Joonsu Jeon <114235515+JIN-JJS@users.noreply.github.com>
2024-03-25 16:19:15 +0900

    [Add] Create Project

---
+++ MapKitTest/MapKitTest/MapKitTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapKitTestApp.swift
+//  MapKitTest
+//
+//  Created by 전준수 on 3/25/24.
+//
+
+import SwiftUI
+
+@main
+struct MapKitTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
