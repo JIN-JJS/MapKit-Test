e775ca1a318fe27a7cb69c956cc057d028524a75
JIN-JJS <joonsu2005@naver.com>
2024-03-25 16:37:44 +0900

    [Add] LocationSearchView

---
+++ MapKitTest/MapKitTest/LocationSearchView.swift
@@ -0,0 +1,18 @@
+//
+//  LocationSearchView.swift
+//  MapKitTest
+//
+//  Created by 전준수 on 3/25/24.
+//
+
+import SwiftUI
+
+struct LocationSearchView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LocationSearchView()
+}
